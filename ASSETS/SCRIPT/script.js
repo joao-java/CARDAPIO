293213b149484f4d110daa9f03ddc30d88ec0f86
João melo <joaomelo_9@hotmail.com>
2022-06-30 22:48:41 -0300

    aaa

--- ASSETS/SCRIPT/script.js
+++ ASSETS/SCRIPT/script.js
@@ -1,7 +1,8 @@
 
 let li = document.querySelectorAll('.menu a' );
+let menu__quantidade = document.querySelectorAll('.menu a span');
 
-console.log(li);
+// ________________________________________________________
 
 li.forEach(function(cada, i){
     console.log(cada, i);
@@ -9,3 +10,19 @@ li.forEach(function(cada, i){
         cada.classList.add("li__reverse");
     }
 });
+
+// ________________________________________________________
+
+// localStorage.setItem('value', document.querySelectorAll(".opcoesOP .card").length);
+// let teste = localStorage.getItem('value');
+// console.log(teste);
+
+let spann = document.querySelectorAll(".opcoesOP .card").length;
+let arma = 0+spann;
+// sessionStorage.setItem('value', spann);
+// let teste1 = sessionStorage.getItem('value');
+// console.log(teste1);
+console.log(arma);
+
+let bla = document.cookie = document.querySelectorAll(".opcoesOP .card");
+console.log(bla);
